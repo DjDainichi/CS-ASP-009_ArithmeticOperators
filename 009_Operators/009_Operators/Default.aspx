fefc09c1cac49b8c18b0fa3ab48b7586c456fb22
Dainichi <dainichi@me.com>
2015-06-27 02:34:12 -0400

    First commit of this Operators project

---
+++ 009_Operators/009_Operators/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_009_Operators.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="inputTextBox" runat="server"></asp:TextBox>
+    
+    </div>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Go Negro" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ 009_Operators/009_Operators/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _009_Operators
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+            int j = 2;
+
+            //int result = i + j;
+            //int result = i - j;
+            //int result = i * j;
+            //int result = i / j;
+
+           // i = i + 1; // 
+           // i += 1; // shorthand for previous operation
+           // i++; // increment by 1
+           // i--; // decrement by 1
+
+            // basic order of operations () * / + -
+            // int myInteger = 5 + 1 * 7; // => output: 12
+            // int myInteger = (5 + 1)*7; // => output: 42
+            // resultLabel.Text = myInteger.ToString();
+
+            // double myDouble = 5.5;
+            // int myInteger = 7;
+            // int myOtherInteger = 4;
+
+
+            //double myResult = myDouble + myInteger; // => output: 12.5 (Implicit Cast)
+            //int myResult = (int)myDouble + myInteger; // => output: 12 (Explicit Cast)
+
+           // int myResult = myInteger / myOtherInteger; // => output: 1 (Truncated)
+           // double myResult = (double)myInteger / (double)myOtherInteger; // => output: 1.75 (Upcast)
+           // resultLabel.Text = myResult.ToString();  
+
+            // int firstNumber = 2000000000; 
+            // int secondNumber = 2000000000;
+            // int result = firstNumber * secondNumber; // => output: -1651507200
+
+            // resultLabel.Text = result.ToString(); // causing an int to 'OVERFLOW'
+
+            int firstNumber = 2000000000;
+            int secondNumber = 2000000000;
+
+            long resultNumber; // declared outside checked scope
+
+            checked 
+            {
+                resultNumber = firstNumber*secondNumber;
+            }
+
+            resultLabel.Text = resultNumber.ToString(); // exposes an overflow exception
+        }
+    }
+}
